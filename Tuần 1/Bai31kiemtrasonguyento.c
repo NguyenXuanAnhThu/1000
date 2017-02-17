d5887a98a5141663c9ee71ed9ab9dce53e4e5345
NguyenXuanAnhThu <thutk08@gmail.com>
2017-02-17 10:07:28 +0700

    Update Bai31kiemtrasonguyento.c

--- Tuần 1/Bai31kiemtrasonguyento.c
+++ Tuần 1/Bai31kiemtrasonguyento.c
@@ -1,19 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-    int i,n;
-    printf("nhap n= ");
-    scanf("%d",&n);
-    for (i=2;i<n;i++)
-    {
-        if(n%i==0)
-        {
-            printf("Khong phai la so nguyen to");
-        }
-        if(n%i==1)
-        {
-            printf("la so nguyen to");
-        }
-    }
 
+    int a,i,dem=0;
+    printf("nhap so a\n");
+    scanf("%d",&a);
+    if (a<2)
+        {
+            printf("a khong phai la so nguyen to\n");
+        }
+    else if (a==2)
+    {
+        printf("a=2 la mot so nguyen to\n");
+
+    }
+    else
+        for (i=2;i<a;i++)
+        if (a%i==0)
+            dem++;
+            if (dem==0)
+                printf("a=%d la mot so nguyen to\n",a);
+            else
+                printf("a=%d khong phai la mot so nguyen to\n",a);
+    getch();
 }
